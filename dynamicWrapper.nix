2baeed519bf8472439ebd4645c864aad22d6b784
Chloe Pou <PouPromC@hopper-dev.unity.local>
2026-07-06 14:28:09 -0400

    fix: retrieve driver version from driver file

--- dynamicWrapper.nix
+++ dynamicWrapper.nix
@@ -15,7 +15,7 @@ writeTextFile rec {
   driver_file=/proc/driver/nvidia/version
 
   vers=$(${gnugrep}/bin/grep "Module" "$driver_file"  |\
-        ${gnused}/bin/sed -E "s/.*Module  ([0-9.]+)  .*/\1/")
+        ${gnugrep}/bin/grep -o "([0-9]{3}.[0-9]{3}.[0-9]{2})" -E)
   if [ -z "$vers" ]; then
     echo "Failed to parse your driver version from $driver_file. Does $driver_file exist?"
     echo "Consider using nixGLNvidia-<version> directly instead."
